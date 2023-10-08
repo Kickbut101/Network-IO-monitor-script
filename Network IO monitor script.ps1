48d7fbda789bc6c33bf313b1b2ff4347f06c7bda
Kickbut101 <bansheedriver400@hotmail.com>
2023-10-08 11:30:44 -0500

    Initial commit

---
+++ Network IO monitor script.ps1
@@ -0,0 +1,7 @@
+﻿$Counter = 0
+do {
+$counter ++
+    $Speed = (Get-CimInstance -Query "Select BytesTotalPersec from Win32_PerfFormattedData_Tcpip_NetworkInterface"| Where {$_.Name -eq 'Realtek PCIe GbE Family Controller'} | Select-Object BytesTotalPerSec).BytesTotalPerSec / 1Mb * 8
+    Write-Host "- $(Get-Date -UFormat %H:%M) - $([math]::round($speed))Mbps"
+    Sleep -Seconds 30
+} while ($counter -le 60)
